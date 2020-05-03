fbb933ecebd70fad309ff94c0afceb0fee09ce57
Alberto Castro <alberto.castro@live.it>
2020-05-03 20:46:15 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # SmartHome
-SmartHome is a small app to switch Tuya smart sockets
+SmartHome is a small app to switch on/off Tuya smart sockets.
+
+![](/images/smarthome.png)
+
+## How to use
+
+- Fill the **Username** and **Password** with the same credentials for the SmartLife app (the username is usually the phone number)
+- For the **CountryCode** use the *International calling code* from [here](https://en.wikipedia.org/wiki/List_of_mobile_telephone_prefixes_by_country)
+- Click **Connect**
+- Use the **On/Off** button to switch on or off the selected socket
+
+> To correctly visualize the statistics (power, ampere, volts) you have to allow the app through the Windows firewall (device discovery is done using udp broadcast)
+
+## Build
+
+You can find a binary build for Windows in the release page.
+
+If you want to run from sources:
+
+1. Create a python virtual env [optional] `python -m venv pyenv`
+2. Install the following requirements:
+    - `pip install pycryptodome`
+    - `pip install pytuya`
+    - `pip install tuyapower`
+    - `pip install requests`
+    - `pip install PyQt5`
+4. Run `python .\control.py`
+
+To build a self contained executable use [pyinstaller](https://www.pyinstaller.org/):
+
+- `pip install pyinstaller`
+- `pyinstaller --name="SmartHome" --windowed --onefile .\control.py`
+
+## Credits
+
+- tuyaha: https://github.com/PaulAnnekov/tuyaha
+- pytuya: https://github.com/clach04/python-tuya
+- tuyapower: https://github.com/jasonacox/tuyapower
---
+++ control.py
@@ -0,0 +1,200 @@
+import tuyapower
+import tuyaha.tuyaapi
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class SmartSocket(object):
+    def __init__(self, id, name, online, state):
+        super().__init__()
+        self.id = id
+        self.name = name
+        self.online = online
+        self.state = state
+        self.ip = None
+        self.key = None
+        self.version = None
+        self.W = None
+        self.mA = None
+        self.V = None
+
+    def __str__(self):
+        return f"Device at {self.ip}: ID {self.id}, state={self.state}, W={self.W}, mA={self.mA}, V={self.V}"
+
+class ScanDeviceThread(QThread):
+    _signal = pyqtSignal(object)
+
+    def __init__(self, callback, parent=None):
+        super().__init__(parent=parent)
+        self._signal.connect(callback)
+
+    def run(self):
+        devices = tuyapower.deviceScan(True)
+        for ip in devices:
+            id = devices[ip]['gwId']
+            self._signal.emit(
+                (id, ip, devices[ip]['productKey'],  devices[ip]['version']))
+        self._signal.emit(None)
+
+class MainWindow(object):
+    def __init__(self):
+        super().__init__()
+        self._app = QApplication([])
+        self._window = QWidget()
+        self._api = tuyaha.TuyaApi()
+        self._selectedSmartSocket = None
+        self._scanDeviceThread = None
+        self._initLayout()
+
+    def _initLayout(self):
+        self._userNameWidget = QLineEdit('')
+        self._passwordWidget = QLineEdit('')
+        self._passwordWidget.setEchoMode(QLineEdit.Password)
+        self._countryCode = QLineEdit('39')
+
+        scanButton = QPushButton('Connect')
+        scanButton.clicked.connect(self._connect)
+        scanButton.setFixedSize(120, 30)
+
+        grid = QGridLayout()
+        grid.addWidget(QLabel('Username'), 0, 0)
+        grid.addWidget(self._userNameWidget, 0, 1)
+
+        grid.addWidget(QLabel('Password'), 0, 2)
+        grid.addWidget(self._passwordWidget, 0, 3)
+
+        grid.addWidget(QLabel('CountryCode'), 0, 4)
+        grid.addWidget(self._countryCode, 0, 5)
+
+        grid.addWidget(scanButton, 1, 0, 1, 6, alignment=Qt.AlignCenter)
+
+        self._deviceList = QListView()
+        self._deviceListModel = QStandardItemModel(self._deviceList)
+        self._deviceList.setModel(self._deviceListModel)
+        self._deviceList.clicked[QModelIndex].connect(self._onItemClicked)
+
+        grid.addWidget(self._deviceList, 2, 0, 2, 5)
+
+        self._NameLabel = QLabel('')
+        self._StateLabel = QLabel('')
+        self._WattLabel = QLabel('')
+        self._AmpereLabel = QLabel('')
+        self._VoltLabel = QLabel('')
+        onOffButton = QPushButton('On / Off')
+        onOffButton.clicked.connect(self._onOff)
+        onOffButton.setFixedSize(120, 30)
+
+        statBox = QVBoxLayout()
+        statBox.addWidget(self._NameLabel)
+        statBox.addWidget(self._StateLabel)
+        statBox.addWidget(self._WattLabel)
+        statBox.addWidget(self._AmpereLabel)
+        statBox.addWidget(self._VoltLabel)
+
+        grid.addLayout(statBox, 2, 5, alignment=Qt.AlignTop | Qt.AlignLeft)
+        grid.addWidget(onOffButton, 3, 5)
+
+        self._window.setWindowTitle('SmartHome')
+        self._window.setLayout(grid)
+
+    def _initUpdate(self):
+        self._timer = QTimer()
+        self._timer.timeout.connect(self._updatePowerInfo)
+        self._timer.start(1000)
+
+    def _updatePowerInfo(self):
+        if self._scanDeviceThread is None:
+            self._scanDeviceThread = ScanDeviceThread(self._handleScanDeviceResult)
+            self._scanDeviceThread.start()
+            return
+        
+        if self._selectedSmartSocket is None:
+            return
+
+        self._NameLabel.setText(f'Name: {self._selectedSmartSocket.name}')
+        self._StateLabel.setText(f'State: {"on" if self._selectedSmartSocket.state else "off"}')
+
+        if self._selectedSmartSocket.key is None:
+            self._WattLabel.setText('Watts: updating...')
+            self._AmpereLabel.setText('mA: updating...')
+            self._VoltLabel.setText('Volts: updating...')
+            return
+
+        (state, W, mA, V, err) = tuyapower.deviceInfo(
+            self._selectedSmartSocket.id,
+            self._selectedSmartSocket.ip,
+            self._selectedSmartSocket.key,
+            self._selectedSmartSocket.version)
+
+        self._selectedSmartSocket.state = state
+        self._selectedSmartSocket.W = W
+        self._selectedSmartSocket.mA = mA
+        self._selectedSmartSocket.V = V
+
+        self._WattLabel.setText(f'Watts: {self._selectedSmartSocket.W}')
+        self._AmpereLabel.setText(f'mA: {self._selectedSmartSocket.mA}')
+        self._VoltLabel.setText(f'Volts: {self._selectedSmartSocket.V}')
+
+    def _handleScanDeviceResult(self, result):
+        if result is None:
+            return
+        (id, ip, key, version) = result
+        for index in range(self._deviceListModel.rowCount()):
+            smartSocket = self._deviceListModel.item(index).data()
+            if smartSocket.id == id:
+                smartSocket.ip = ip
+                smartSocket.key = key
+                smartSocket.version = version
+                break
+
+    def _connect(self):
+        username = self._userNameWidget.text()
+        password = self._passwordWidget.text()
+        countryCode = self._countryCode.text()
+
+        if len(username) == 0 or len(password) == 0 or len(countryCode) == 0:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setText("Please insert login data")
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec_()
+            return
+
+        self._api.init(username, password, countryCode, 'smart_life')
+
+        devices = self._api.discover_devices()
+
+        self._deviceListModel.clear()
+
+        for device in devices:
+            smartSocket = SmartSocket(
+                device['id'], device['name'], device['data']['online'], device['data']['state'])
+            item = QStandardItem(
+                f'[{"Online" if smartSocket.online else "Offline":7}] {smartSocket.name}')
+            item.setFont(QFont('Consolas'))
+            item.setData(smartSocket)
+            self._deviceListModel.appendRow(item)
+        
+        self._initUpdate()
+
+    def _onItemClicked(self, index):
+        item = self._deviceListModel.itemFromIndex(index)
+        self._selectedSmartSocket = item.data()
+        
+    def _onOff(self):
+        if self._selectedSmartSocket is None:
+            return
+        device = self._api.get_device_by_id(self._selectedSmartSocket.id)
+        if self._selectedSmartSocket.state:
+            device.turn_off()
+        else:
+            device.turn_on()
+        self._selectedSmartSocket.state = not self._selectedSmartSocket.state
+
+    def show(self):
+        self._window.show()
+        self._app.exec_()
+
+if __name__ == "__main__":
+    mainWindow = MainWindow()
+    mainWindow.show()
